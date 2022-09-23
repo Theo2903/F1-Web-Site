c4c52fdaa2f77c0c3d183419b27eb832854647ef
Théo Ribbi <theoribb@gmail.com>
2022-09-23 10:36:20 +0200

    add more informations to races

--- src/views/ScheduleInfoView.vue
+++ src/views/ScheduleInfoView.vue
@@ -1,6 +1,34 @@
 <template>
-    <div>
-        test 
+    <div v-for="race in schedule_info" :key="race.id">
+        <!-- Info -->
+        <a> Info race </a><br>
+        <a>{{ race.raceName }}</a><br>
+        <a>{{ race.round }}</a><br>
+        <!-- Location -->
+        <a>Location</a><br>
+        <a>{{ race.Circuit.Location.lat }}</a><br>
+        <a>{{ race.Circuit.Location.long }}</a><br>
+        <a>{{ race.Circuit.Location.locality }}</a><br>
+        <!-- First Practice -->
+        <a>First Practice</a><br>
+        <a>{{ race.FirstPractice.date }} &nbsp;</a>
+        <a>{{ race.FirstPractice.time }}</a><br>
+        <!-- Second Practice -->
+        <a>Second Practice</a><br>
+        <a>{{ race.SecondPractice.date }} &nbsp;</a>
+        <a>{{ race.SecondPractice.time }}</a><br>
+        <!-- Third Practice -->
+        <a>Third Practice</a><br>
+        <a>{{ race.ThirdPractice.date }} &nbsp;</a>
+        <a>{{ race.ThirdPractice.time }}</a><br>
+        <!-- Qualifying -->
+        <a>Qualifying</a><br>
+        <a>{{ race.Qualifying.date }} &nbsp;</a>
+        <a>{{ race.Qualifying.time }}</a><br>
+        <!-- Race -->
+        <a>Race</a><br>
+        <a>{{ race.date }} &nbsp;</a>
+        <a>{{ race.time }}</a><br>   
     </div>    
 </template>
   
@@ -11,8 +39,8 @@
     
       data: () => ({
         api_url: "http://ergast.com/api/f1/",
-        api_schedule: "current.json",
-        schedules_list: [],
+        api_schedule: "current/",
+        schedule_info: [],
       }),
     
       methods: {
@@ -24,8 +52,8 @@
       },
     
       mounted() {
-        axios.get(this.api_url + this.api_schedule).then((response) => {
-          this.schedules_list = response.data.MRData.RaceTable.Races
+        axios.get(this.api_url + this.api_schedule + this.$route.params.round +".json").then((response) => {
+          this.schedule_info = response.data.MRData.RaceTable.Races
         }); 
       },
     }
