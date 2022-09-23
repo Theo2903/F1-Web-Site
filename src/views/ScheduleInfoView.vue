45c86ce465df990538a272f76bebae9ff57500ec
Théo Ribbi <theoribb@gmail.com>
2022-09-23 10:15:57 +0200

    add components for a schedule informations

---
+++ src/views/ScheduleInfoView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        test 
+    </div>    
+</template>
+  
+<script>
+    import axios from "axios";
+    
+    export default { 
+    
+      data: () => ({
+        api_url: "http://ergast.com/api/f1/",
+        api_schedule: "current.json",
+        schedules_list: [],
+      }),
+    
+      methods: {
+    
+        go_to_raceinfo(races){
+          this.$router.push({ path: 'schedule/' + races.round})
+        },
+    
+      },
+    
+      mounted() {
+        axios.get(this.api_url + this.api_schedule).then((response) => {
+          this.schedules_list = response.data.MRData.RaceTable.Races
+        }); 
+      },
+    }
+    
+</script>
