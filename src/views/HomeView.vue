9a58e0b262eb84b8cfc69344001ea3b84ae6f699
Theo Ribbi <theoribb@gmail.com>
2022-09-26 19:47:37 +0200

    delete class not used

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overlay relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+  <div class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
     <video autoplay loop muted class="brightness-50 absolute z-10 w-auto min-w-full min-h-full max-w-none" >
       <source src="../assets/F12022Opening.mp4" />
     </video>
