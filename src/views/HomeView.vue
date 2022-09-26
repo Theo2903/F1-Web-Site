7f8cb156526ea41eb8839b767aa4df98295d999e
Theo Ribbi <theoribb@gmail.com>
2022-09-26 19:41:42 +0200

    formatted code and add generic video

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home">
-    <p> home </p>
+  <div class="overlay relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+    <video autoplay loop muted class="brightness-50 absolute z-10 w-auto min-w-full min-h-full max-w-none" >
+      <source src="../assets/F12022Opening.mp4" />
+    </video>
   </div>
 </template>
 
+<style>
+/* .overlay {
+  background-image: rgba(0, 0, 0, 1);
+  opacity: .8;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+} */
+
+</style>
