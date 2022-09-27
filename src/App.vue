2e4879d57efae6bab7a0e6be79f40154df821005
Theo Ribbi <theoribb@gmail.com>
2022-09-27 19:42:52 +0200

    add formula 1 logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <NavBar/>
-  <div class="flex top-0 justify-end m-2 fixed bg-transparent text-white w-full pr-10 pt-2">
-    <i class="fas fa-bars fa-2x"></i>
+  <div class="flex top-0 justify-end m-2 fixed text-white w-full pr-10 pt-2">
+    <i @click="open_nav()" class="fas fa-bars fa-2x"></i>
+  </div>
+  <div class="flex top-0 justify-center m-2 fixed text-white w-full pr-10 pt-2">
+    <img class="h-8 w-18" src="@/assets/formula-1-logo.png" alt="">
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavbarComp.vue'
+import NavBar from '@/components/SideBar.vue'
 
 export default {
+
   components: {
     NavBar
+  },
+  methods: {
+    open_nav () {
+      console.log('gniaaa')
+    }
   }
 }
 
