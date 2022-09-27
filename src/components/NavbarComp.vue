c06757590955947bbdbe56f63edcecd890e4c0de
Theo Ribbi <theoribb@gmail.com>
2022-09-27 19:17:13 +0200

    comment all

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,29 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/drivers">Drivers</router-link> |
-    <router-link to="/schedule">Schedule</router-link> |
+  <!-- <nav class="flex justify-around m-2">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/drivers">Drivers</router-link>
+    <router-link to="/">
+      <img class="h-10 w-full" src="@/assets/formula-1-logo.png" alt="f1-logo">
+    </router-link>
+    <router-link to="/schedule">Schedule</router-link>
     <router-link to="/team">Team</router-link>
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-nav {
-  padding: 30px;
-}
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
