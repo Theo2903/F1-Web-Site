4236b4e3dcd876c4e2bd1e5b9e4357453f640176
Theo Ribbi <theoribb@gmail.com>
2022-09-27 19:23:23 +0200

    add brightness to video

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-    <video id="myVideo" autoplay loop muted class="fixed right-0 bottom-0 min-w-full min-h-full" >
+    <video id="myVideo" autoplay loop muted class="brightness-50 fixed right-0 bottom-0 min-w-full min-h-full" >
       <source src="../assets/F12022Opening.mp4" />
     </video>
   </div>
