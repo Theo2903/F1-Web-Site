55aed5635c30a3dc4a4e0e1415c287b897d7ebe6
Theo Ribbi <theoribb@gmail.com>
2022-09-27 19:43:24 +0200

    rename components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <nav class="flex justify-around m-2">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/drivers">Drivers</router-link>
+    <router-link to="/">
+      <img class="h-10 w-full" src="@/assets/formula-1-logo.png" alt="f1-logo">
+    </router-link>
+    <router-link to="/schedule">Schedule</router-link>
+    <router-link to="/team">Team</router-link>
+  </nav> -->
+  <router-view/>
+</template>
