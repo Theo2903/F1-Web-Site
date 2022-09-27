30a2bfe442f8e1dd42de25fdc3b771cc73b0f9d7
Theo Ribbi <theoribb@gmail.com>
2022-09-27 19:17:31 +0200

    change css class

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-    <video autoplay loop muted class="brightness-50 absolute z-10 w-auto min-w-full min-h-full max-w-none" >
+    <video id="myVideo" autoplay loop muted class="fixed right-0 bottom-0 min-w-full min-h-full" >
       <source src="../assets/F12022Opening.mp4" />
     </video>
   </div>
